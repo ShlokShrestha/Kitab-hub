ad2171348c19cc7f1966d4329d7bcaf716efebba
Shlok <shlokstha99@gmail.com>
2024-01-11 23:32:06 +0545

    feat: update in books page

--- src/app/books/page.js
+++ src/app/books/page.js
@@ -11,6 +11,7 @@ import React from "react";
 import BookCard from "@/components/BookCard";
 const page = async () => {
   const data = await getAllBook();
+  
   return (
     <Container>
       <Box>
